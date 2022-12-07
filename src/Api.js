7bf5c86305463655d5d71f7ac5c639e8317ac7bf
guy0017 <waesin@gmail.com>
2022-12-07 14:37:24 +0000

    changed API url address to new host

--- src/Api.js
+++ src/Api.js
@@ -1,20 +1,18 @@
 const axios = require("axios");
+const hostDomain = "https://ncnews-guy.cyclic.app/api";
 
 export const getArticles = (sortBy, order) => {
   return axios
-    .get(
-      `https://ncnews-guy.herokuapp.com/api/articles?sortBy=${sortBy}&&order=${order}`
-    )
+    .get(`${hostDomain}/articles?sortBy=${sortBy}&&order=${order}`)
     .then(({ data }) => {
       return data.articles;
-    })
+    });
 };
 
 export const getArticlesByTopic = (topic, sortBy, order) => {
   return axios
-
     .get(
-      `https://ncnews-guy.herokuapp.com/api/articles?topic=${topic}&&sortBy=${sortBy}&&order=${order}`
+      `${hostDomain}/api/articles?topic=${topic}&&sortBy=${sortBy}&&order=${order}`
     )
     .then(({ data }) => {
       return data.articles;
@@ -23,52 +21,49 @@ export const getArticlesByTopic = (topic, sortBy, order) => {
 
 export const getAllTopics = () => {
   return axios
-    .get("https://ncnews-guy.herokuapp.com/api/topics")
-    .then(({ data }) => {
-      return data.topics;
-    });
+    .get(`${hostDomain}/api/topics`).then(({ data }) => {
+    return data.topics;
+  });
 };
 
 export const getArticleByID = (article_id) => {
   return axios
-    .get(`https://ncnews-guy.herokuapp.com/api/articles/${article_id}`)
+    .get(`${hostDomain}/api/articles/${article_id}`)
     .then(({ data }) => {
       return data.articles[0];
     });
 };
 
 export const patchVote = (userVote, article_id) => {
-  return axios.patch(
-    `https://ncnews-guy.herokuapp.com/api/articles/${article_id}`,
-    { inc_votes: userVote }
-  );
+  return axios.patch(`${hostDomain}/api/articles/${article_id}`, {
+    inc_votes: userVote,
+  });
 };
 
 export const getCommentsByID = (article_id) => {
   return axios
-    .get(`https://ncnews-guy.herokuapp.com/api/articles/${article_id}/comments`)
+    .get(`${hostDomain}/api/articles/${article_id}/comments`)
     .then(({ data }) => {
       return data.comments;
     });
 };
 
 export const postCommentByID = (user, addCommentInput, article_id) => {
-  return axios.post(
-    `https://ncnews-guy.herokuapp.com/api/articles/${article_id}/comments`,
-    { username: user, body: addCommentInput }
-  );
+  return axios
+    .post(`${hostDomain}/api/articles/${article_id}/comments`, {
+    username: user,
+    body: addCommentInput,
+  });
 };
 
 export const deleteCommentByCommentID = (comment_id) => {
-  return axios.delete(
-    `https://ncnews-guy.herokuapp.com/api/comments/${comment_id}`
-  );
+  return axios
+    .delete(`${hostDomain}/api/comments/${comment_id}`);
 };
 
 export const getUsers = () => {
   return axios
-    .get("https://ncnews-guy.herokuapp.com/api/users")
-    .then(({ data }) => {
-      return data.users;
-    });
+    .get(`${hostDomain}/api/users`).then(({ data }) => {
+    return data.users;
+  });
 };
